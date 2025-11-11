e66b44f3a4169abedbedc5052b189edad487d5b4
squick <ramzi.idir@proton.me>
2025-11-11 06:10:53 +0100

    v.0.2.4 fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,11 @@ import '../assets/css/home.css';
 export default function Home() {
     return (
         <div className="container">
-            <img src="./assets/img/logo.webp" alt="Logo du portfolio" />
+            <img src="../public/img/logo.webp" alt="Logo du portfolio" />
             <div className="hero">
                 <div className="hero_head">
                     <h1>salut monde !</h1>      
-                    <img class="globe_emoji" src="./public/img/icons/globe.gif" alt="Earth globe in 3D rotating on itself"></img>
+                    <img class="globe_emoji" src="../public/img/icons/globe.gif" alt="Earth globe in 3D rotating on itself"></img>
                 </div>
                 <TypeAnimation
                     sequence={[
