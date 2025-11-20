b867050cbf2b8f0aa7cad97870c9e93e857b9ca2
squick <ramzi.idir@proton.me>
2025-11-20 03:00:04 +0100

    v.0.2.6 skills + navbar update

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,7 +1,19 @@
+import Magnet from '../components/elements/Magnet.jsx'
+import ScrollReveal from '../components/ScrollReveal.jsx';
+import '../assets/css/skills.css';
+
 export default function Skills(){
     return(
         <div className="Skillspage">
-            <h1>des compétences du front au back.</h1>
+            <ScrollReveal
+                baseOpacity={0}
+                enableBlur={true}
+                baseRotation={5}
+                blurStrength={4}
+                textClassName="h1-skills"
+            >
+                développer l'idée dans mon cahier et l'imaginer sur Figma pour lui donner forme avec React et enfin la vie avec PhP 
+            </ScrollReveal>
         </div>
     )
 }
